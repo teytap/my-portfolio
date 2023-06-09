0c5a48f4e9c68c22a7ded58edc82b137014cc897
Mehtap Tataroğlu <teytap@gmail.com>
2023-06-09 21:06:45 +0300

    updated navbar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -19,7 +19,9 @@ const Layout = () => {
               <Nav.Link href="/">Home</Nav.Link>
             </Nav.Item>
 
-            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/about" to="/about">
+              About
+            </Nav.Link>
 
             <Nav.Link href="/work">Work</Nav.Link>
 
